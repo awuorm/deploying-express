e1a0920171db05f0503513efe2f4d57ce454156a
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-31 13:53:53 +0300

    Added default page on server

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(express.static(__dirname + "/client/build"));
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/client/build/index.html");
+})
+
 app.get('/api/friends', (req, res, next) => {
   res.json(friends);
 });
